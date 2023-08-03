28239dee223178ae9e8601a8b8cf3a6bd1652dd8
ig0r <igormedeiros.f@gmail.com>
2023-08-02 21:42:04 -0300

    navbar

---
+++ clone-airbnb/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
